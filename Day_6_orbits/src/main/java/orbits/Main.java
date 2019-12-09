8c683d3df45fa670073aa4a889d1156d0de8ff62
i332098 <peter.vekony@sap.com>
2019-12-09 09:38:54 +0100

    Solution for Day 6 Part 2

--- Day_6_orbits/src/main/java/orbits/Main.java
+++ Day_6_orbits/src/main/java/orbits/Main.java
@@ -1,15 +1,15 @@
 package orbits;
 
-import orbits.domain.SpaceObject;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import orbits.domain.SpaceObject;
 
 public class Main {
 
@@ -29,8 +29,39 @@ public class Main {
 
         map.values().forEach(spaceObject -> spaceObject.setObjectToOrbit(map.get(spaceObject.getObjectToOrbitId())));
         Integer totalOrbits = map.values().stream().reduce(0, (identity, object) -> identity + object.getNumberOfOrbits(), Integer::sum);
-        System.out.print("PART 1.: TOTAL ORBITS: " + totalOrbits);
+        System.out.println("PART 1.: TOTAL ORBITS: " + totalOrbits);
 
+        SpaceObject youOrbit = map.get("YOU").getObjectToOrbit();
+        SpaceObject sanOrbit = map.get("SAN").getObjectToOrbit();
+        Integer commonOrbitedObjectNumber = getCommonOrbitedObject(youOrbit, sanOrbit).getNumberOfOrbits();
+        Integer transfersNeeded = (youOrbit.getNumberOfOrbits() - commonOrbitedObjectNumber) + (sanOrbit.getNumberOfOrbits() - commonOrbitedObjectNumber);
 
+        System.out.println("PART 2.: TRANSFERS NEEDED: " + transfersNeeded);
+    }
+
+    private static SpaceObject getCommonOrbitedObject(SpaceObject so1, SpaceObject so2) {
+        List<String> ids = new ArrayList<>();
+        while (true) {
+            if (so1.getObjectToOrbit() != null) {
+                String id1 = so1.getId();
+                if (ids.contains(id1)) {
+                    return so1;
+                }
+                else {
+                    ids.add(id1);
+                    so1 = so1.getObjectToOrbit();
+                }
+            }
+            if (so2.getObjectToOrbit() != null) {
+                String id2 = so2.getId();
+                if (ids.contains(id2)) {
+                    return so2;
+                }
+                else {
+                    ids.add(id2);
+                    so2 = so2.getObjectToOrbit();
+                }
+            }
+        }
     }
 }
