00074ea3c6f33bb811f75a835bc9c26040723071
i332098 <peter.vekony@sap.com>
2019-12-25 10:30:21 +0100

    Init Day 13

---
+++ Day_13_Care_Package/src/main/java/day_13/Main.java
@@ -0,0 +1,17 @@
+package day_13;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+  public static void main(String[] args) throws URISyntaxException, IOException {
+    String input =
+        Files.readString(
+            Paths.get(Main.class.getResource("/day_13_input.txt").toURI()), StandardCharsets.UTF_8);
+    System.out.println(input);
+  }
+}
